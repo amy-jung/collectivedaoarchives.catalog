1fd9b279ecc20f18a05987aed719dfc851e3478f
Carlos Sánchez <oceanrdn@gmail.com>
2023-06-06 11:43:46 +0200

    DaisyUI + random default theme

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-3xl font-bold">Collective DAO Archive Catalog</h1>
+      <p>An open source index of DAO historical events.</p>
+      <button className="btn btn-primary mt-10">Coming Soon!</button>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  plugins: [require("daisyui")],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+  daisyui: {
+    themes: [
+      {
+        collectiveDao: {
+          primary: "#65c3c8",
+          secondary: "#ef9fbc",
+          accent: "#eeaf3a",
+          neutral: "#291334",
+          "base-100": "#faf7f5",
+          "base-200": "#efeae6",
+          "base-300": "#e7e2df",
+          "base-content": "#291334",
+        },
       },
-    },
+      "dark",
+    ],
   },
-  plugins: [],
+  theme: {},
 };
