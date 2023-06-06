6cf13e58b12e3ea94e9aa9f1b6008f97c1c7e180
Carlos Sánchez <oceanrdn@gmail.com>
2023-06-06 11:22:32 +0200

    Add prettier + config

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-3xl font-bold">Collective DAO Archive Catalog</h1>
     </main>
-  )
+  );
 }
