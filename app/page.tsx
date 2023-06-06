a5311cddd33dbd0ee785b9ba4f3e30337f54445a
Carlos Sánchez <oceanrdn@gmail.com>
2023-06-06 11:58:54 +0200

    UI tweaks (test Vercel deployment)

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-3xl font-bold">Collective DAO Archive Catalog</h1>
-      <p>An open source index of DAO historical events.</p>
+    <main className="flex min-h-screen flex-col items-center p-8 md:p-24">
+      <h1 className="text-3xl font-bold text-center">Collective DAO Archive Catalog</h1>
+      <p className="text-center">An open source index of DAO historical events.</p>
       <button className="btn btn-primary mt-10">Coming Soon!</button>
     </main>
   );
