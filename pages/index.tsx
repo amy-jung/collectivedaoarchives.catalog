6c8fe2e89a41aa9727b81199b65bef50ddd0a2ab
Carlos Sánchez <oceanrdn@gmail.com>
2023-06-13 17:53:52 +0200

    POC search

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Link from "next/link";
 import type { GetStaticProps, NextPage } from "next";
 import { CatalogRecord, getAllRecords } from "~~/utils/records";
@@ -7,24 +8,39 @@ type CatalogProps = {
 };
 
 const Home: NextPage<CatalogProps> = ({ records }) => {
+  const [search, setSearch] = useState("");
+
+  const filteredRecords = records.filter(record => record.title.toLowerCase().includes(search.toLowerCase()));
+
   return (
-    <div className="flex flex-col items-center p-8 md:p-24">
+    <div className="flex flex-col items-center p-8 md:px-24">
       <h1 className="text-4xl font-bold text-center">Collective DAO Archive Catalog</h1>
       <p className="text-center">An open source index of DAO historical events.</p>
-      <h2 className="font-bold text-3xl mt-12 my-4">Catalog</h2>
-      <div className="flex flex-col gap-8">
-        {records.map(record => (
-          <div key={record.slug}>
-            <h2 className="text-xl mb-2">
-              <Link href={`/records/${record.slug}`}>
-                <span className="font-bold link">{record.title}</span>
-              </Link>
-            </h2>
-            <p>
-              {record.summary} / <span className="italic">{record.protocol}</span>
-            </p>
-          </div>
-        ))}
+      <h2 className="font-bold text-3xl mt-12">Catalog</h2>
+      <input
+        className="mt-4 mb-8 p-2 border border-gray-300"
+        type="text"
+        placeholder="Search records..."
+        value={search}
+        onChange={e => setSearch(e.target.value)}
+      />
+      <div className="flex flex-col gap-8 w-[600px]">
+        {search.length >= 3 && filteredRecords.length === 0 ? (
+          <p>No results found</p>
+        ) : (
+          filteredRecords.map(record => (
+            <div key={record.slug}>
+              <h2 className="text-xl mb-2">
+                <Link href={`/records/${record.slug}`}>
+                  <span className="font-bold link">{record.title}</span>
+                </Link>
+              </h2>
+              <p>
+                {record.summary} / <span className="italic">{record.protocol}</span>
+              </p>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
