0c2b58ec5f47e4e5f500992d08995ffc88e07d5d
Carlos Sánchez <oceanrdn@gmail.com>
2023-06-30 15:19:02 +0200

    Import script + index pagination (#5)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,39 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { PrismaClient, Record } from "@prisma/client";
 import type { GetServerSideProps, NextPage } from "next";
 
 interface RecordsProps {
   records: Record[];
+  totalCount: number;
 }
 
-const Home: NextPage<RecordsProps> = ({ records }) => {
+const PAGE_SIZE = 10;
+
+const Home: NextPage<RecordsProps> = ({ records, totalCount }) => {
+  const router = useRouter();
+  const currentPage = Number(router.query.page) || 1;
+
+  console.log("records", records);
+
+  // Calculate the total number of pages
+  const totalPages = Math.ceil(totalCount / PAGE_SIZE);
+
+  // Calculate the start and end indexes of the current page
+  const startIndex = (currentPage - 1) * PAGE_SIZE;
+  const endIndex = startIndex + PAGE_SIZE;
+
+  const goToPage = (page: number) => {
+    router.push(`/?page=${page}`);
+  };
+
   return (
     <div className="flex flex-col items-center p-8 md:px-24">
       <h1 className="text-4xl font-bold text-center">Collective DAO Archive Catalog</h1>
       <p className="text-center">An open source index of DAO historical events.</p>
-      <h2 className="font-bold text-3xl mt-12 mb-6">Catalog</h2>
+      <h2 className="font-bold text-3xl mt-12 mb-6">
+        Catalog <span>({totalCount})</span>
+      </h2>
       <div className="flex flex-col gap-8 w-[600px]">
         {records?.map(record => (
           <div key={record.id}>
@@ -21,21 +43,56 @@ const Home: NextPage<RecordsProps> = ({ records }) => {
               </Link>
             </h2>
             <p>
-              {record.summary} / <span className="italic">{record.organization}</span>
+              <span className="italic">{record.organization}</span>
             </p>
           </div>
         ))}
+      </div>
+      <div className="flex justify-between items-center mt-8 gap-4">
+        <button
+          className={`btn btn-primary ${currentPage === 1 ? "cursor-not-allowed" : ""}`}
+          onClick={() => {
+            if (currentPage > 1) {
+              goToPage(currentPage - 1);
+            }
+          }}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+
+        <p className="text-gray-700 flex flex-col gap-2">
+          Page {currentPage} of {totalPages}
+        </p>
+
+        <button
+          className={`btn btn-primary ${currentPage === totalPages ? "cursor-not-allowed" : ""}`}
+          onClick={() => {
+            if (currentPage < totalPages) {
+              goToPage(currentPage + 1);
+            }
+          }}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async context => {
   const prisma = new PrismaClient();
+  const currentPage = Number(context.query.page) || 1;
   let records: Record[];
+  let totalCount: number = 0;
 
   try {
-    records = await prisma.record.findMany();
+    const offset = (currentPage - 1) * PAGE_SIZE;
+    [records, totalCount] = await Promise.all([
+      prisma.record.findMany({ take: PAGE_SIZE, skip: offset }),
+      prisma.record.count(),
+    ]);
   } catch (err) {
     console.log(err);
     records = [];
@@ -53,7 +110,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   }));
 
   return {
-    props: { records: serializedRecords },
+    props: { records: serializedRecords, totalCount },
   };
 };
 
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,94 +1,127 @@
 import { PrismaClient } from "@prisma/client";
+import csv from "csv-parser";
+import fs from "fs";
+import path from "path";
 
 const prisma = new PrismaClient();
 
-// Seed Example
+type CsvRecord = {
+  id: number;
+  title: string;
+  protocol: string;
+  link: string;
+  category: string;
+  // ToDo. This will be multiple
+  subcategories: string;
+  // ToDo. Missing fields (date, etc)
+};
+
+const generateSlug = (title: string): string => {
+  return title.toLowerCase().replace(/ /g, "-");
+};
+
 async function main() {
-  await prisma.subCategory.upsert({
-    where: { id: 1 },
-    update: {},
-    create: {
-      name: "Onboarding",
-    },
-  });
+  if (!fs.existsSync(path.join(__dirname, "records.csv"))) {
+    console.error("\nrecords.csv not found, skipping migration");
+    return;
+  }
 
-  await prisma.subCategory.upsert({
-    where: { id: 2 },
-    update: {},
-    create: {
-      name: "Grant",
-    },
-  });
+  const records: CsvRecord[] = [];
+  fs.createReadStream(path.join(__dirname, "records.csv"))
+    .pipe(csv())
+    .on("data", data => records.push(data))
+    .on("end", async () => {
+      console.log("records.csv parsed, seeding database with", records.length, "records");
+      // Dedupe and create categories and subcategories first (and remove empty subcategories)
+      const categories = [
+        ...new Set(records.filter(record => record.category.trim() !== "").map(record => record.category)),
+      ];
+      const subcategories = [
+        ...new Set(records.filter(record => record.subcategories.trim() !== "").map(record => record.subcategories)),
+      ];
 
-  await prisma.category.upsert({
-    where: { id: 1 },
-    update: {},
-    create: {
-      id: 1,
-      name: "Organizational",
-    },
-  });
+      // Saving mapping on memory to save some queries
+      const categoryMap: { [key: string]: number } = {};
+      const subcategoryMap: { [key: string]: number } = {};
 
-  await prisma.record.upsert({
-    where: { id: 1 },
-    update: {},
-    create: {
-      id: 1,
-      title: "Guiding Principles",
-      slug: "guiding-principles",
-      date: new Date("2023-06-09"),
-      organization: "index",
-      link: "https://docs.indexcoop.com/index-coop-community-handbook/our-guiding-principles",
-      summary: "This is the summary text for the Index Coop Guiding Principles.",
-      categoryId: 1,
-      subcategories: {
-        create: [{ subCategoryId: 1 }, { subCategoryId: 2 }],
-      },
-    },
-  });
+      console.log("Creating categories");
+      for (const categoryName of categories) {
+        const category = await prisma.category.upsert({
+          where: { name: categoryName },
+          update: {},
+          create: { name: categoryName },
+        });
 
-  await prisma.record.upsert({
-    where: { id: 2 },
-    update: {},
-    create: {
-      id: 2,
-      title: "Principles for Operational Excellence",
-      slug: "principles-for-operational-excellence",
-      date: new Date("2023-06-10"),
-      organization: "index",
-      link: "https://gov.indexcoop.com/t/10-principles-for-operational-excellence-at-the-index-coop/4369",
-      summary: "10 Principles for Operational Excellence at the Index Coop\n" + "category: Organizational",
-      categoryId: 1,
-      subcategories: {
-        create: [{ subCategoryId: 1 }],
-      },
-    },
-  });
+        categoryMap[category.name] = category.id;
+      }
 
-  await prisma.record.upsert({
-    where: { id: 3 },
-    update: {},
-    create: {
-      id: 3,
-      title: "Conflict Resolution Process",
-      slug: "conflict-resolution-process",
-      date: new Date("2023-06-11"),
-      organization: "index",
-      link: "https://gov.indexcoop.com/t/conflict-management-framework-and-tools/3751",
-      summary: "Conflict Management - Framework and Tools",
-      categoryId: 1,
-      subcategories: {
-        create: [{ subCategoryId: 2 }],
-      },
-    },
-  });
+      console.log("Creating subcategories");
+      for (const subcategoryName of subcategories) {
+        const subcategory = await prisma.subCategory.upsert({
+          where: { name: subcategoryName },
+          update: {},
+          create: { name: subcategoryName },
+        });
+
+        subcategoryMap[subcategory.name] = subcategory.id;
+      }
+
+      // Then create records
+      console.log("Creating Records");
+      for (const record of records) {
+        console.log("Creating Record", record.title);
+
+        // Generate slug and ensure it's unique
+        let slugAuto = generateSlug(record.title);
+        let count = 1;
+        while (await prisma.record.findFirst({ where: { slug: slugAuto } })) {
+          slugAuto = `${generateSlug(record.title)}-${count}`;
+          count++;
+        }
+
+        let recordData = {
+          id: Number(record.id),
+          title: record.title,
+          slug: slugAuto,
+          date: new Date(),
+          organization: record.protocol,
+          link: record.link,
+          summary: "",
+        } as {
+          id: number;
+          title: string;
+          slug: string;
+          date: Date;
+          organization: string;
+          link: string;
+          categoryId?: number;
+          summary: string;
+          subcategories?: { create: { subCategoryId: number }[] };
+        };
+
+        if (categoryMap[record.category]) {
+          recordData.categoryId = categoryMap[record.category];
+        }
+
+        if (subcategoryMap[record.subcategories]) {
+          recordData.subcategories = {
+            create: [{ subCategoryId: subcategoryMap[record.subcategories] }],
+          };
+        }
+
+        await prisma.record.upsert({
+          where: { id: Number(record.id) },
+          update: {},
+          create: recordData,
+        });
+      }
+
+      await prisma.$disconnect();
+    });
 }
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async e => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+
+main().catch(async e => {
+  console.error(e);
+  await prisma.$disconnect();
+  process.exit(1);
+});
