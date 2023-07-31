181009c2eac056b0c95d90ec5cb6e6e98631a236
Carlos Sánchez <oceanrdn@gmail.com>
2023-07-31 18:14:26 +0200

    Main page records. RWD index + header

--- packages/frontend/components/Header.tsx
+++ packages/frontend/components/Header.tsx
@@ -12,13 +12,13 @@ export const Header = () => {
   return (
     <div className="flex justify-between items-center p-4 bg-secondary">
       {/* hide this span if we are in the NextJS main page */}
-      <span className="font-bold text-xl">
+      <span className="font-bold sm:text-xl">
         <Link href="/" className={isMainPage ? "hidden" : ""}>
           DAO COLLECTIVE CATALOG
         </Link>
       </span>
-      <div className="flex gap-12 items-center">
-        <ul className="flex gap-12 text-xl font-bold">
+      <div className="flex gap-6 sm:gap-12 items-center">
+        <ul className="flex gap-6 sm:gap-12 sm:text-xl font-bold">
           <li>
             <Link href="/records">Records</Link>
           </li>
