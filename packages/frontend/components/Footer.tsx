d2c27a9547e32e7eefb43a8c310972b3f657ad44
Carlos Sánchez <oceanrdn@gmail.com>
2023-07-31 19:07:44 +0200

    RWD tweaks

--- packages/frontend/components/Footer.tsx
+++ packages/frontend/components/Footer.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export const Footer = () => {
   return (
     <div className="flex items-center p-4 py-5 bg-secondary mt-20">
-      <Link href="/about" className="font-bold text-xl">
+      <Link href="/about" className="font-bold sm:text-xl">
         About
       </Link>
     </div>
