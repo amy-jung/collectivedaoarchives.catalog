b438bc9d647e52c20945a396e5f0aa455cabe83b
Carlos Sánchez <oceanrdn@gmail.com>
2023-07-31 17:35:57 +0200

    Inter typo

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 import { WagmiConfig } from "wagmi";
@@ -6,11 +7,13 @@ import { Header } from "~~/components/Header";
 import { chains, wagmiConfig } from "~~/services/web3/wagmiConnectors";
 import "~~/styles/globals.css";
 
+const inter = Inter({ subsets: ["latin"] });
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <WagmiConfig config={wagmiConfig}>
       <RainbowKitProvider chains={chains}>
-        <div className="flex flex-col min-h-screen">
+        <div className={`flex flex-col min-h-screen ${inter.className}`}>
           <Header />
           <main className="relative flex flex-col flex-1">
             <Component {...pageProps} />
