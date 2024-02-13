ab6f54cb11cd74da0e9df5d3a5927c77d73586c3
Carlos Sánchez <oceanrdn@gmail.com>
2024-02-13 21:22:15 +0100

    Burger menu

--- packages/frontend/components/Header.tsx
+++ packages/frontend/components/Header.tsx
@@ -1,7 +1,10 @@
+import { useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { BurgerIcon } from "~~/components/icons/BurgerIcon";
+import { CloseIcon } from "~~/components/icons/CloseIcon";
 
 /**
  * Site header
@@ -10,6 +13,13 @@ export const Header = () => {
   const pathname = usePathname();
   const isMainPage = pathname === "/";
   const isContributePage = pathname === "/contribute";
+  const drawerCheckboxRef = useRef<HTMLInputElement>(null);
+
+  const closeDrawer = () => {
+    if (drawerCheckboxRef.current) {
+      drawerCheckboxRef.current.checked = false;
+    }
+  };
 
   return (
     <div className="flex justify-between items-center px-12 py-6 bg-accent">
@@ -23,7 +33,7 @@ export const Header = () => {
           </span>
         </Link>
       </span>
-      <div className="flex gap-6 sm:gap-12 items-center">
+      <div className="hidden lg:flex gap-6 sm:gap-12 items-center">
         <ul className="flex gap-6 sm:gap-12 sm:text-2xl font-bold uppercase">
           <li>
             <Link href="/search">Search</Link>
@@ -40,6 +50,40 @@ export const Header = () => {
             <ConnectButton chainStatus="icon" label="Connect" />
           </div>
         )}
+      </div>
+      <div className="drawer drawer-end text-right lg:hidden">
+        <input id="menu-drawer" type="checkbox" className="drawer-toggle" ref={drawerCheckboxRef} />
+        <div className="drawer-content">
+          <label htmlFor="menu-drawer" className="drawer-button flex justify-end">
+            <BurgerIcon />
+          </label>
+        </div>
+        <div className="drawer-side z-50">
+          <label htmlFor="menu-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+          <div className="p-4 min-w-[200px] min-h-full bg-base-100 text-base-content text-left">
+            <span onClick={closeDrawer} className="flex justify-end">
+              <CloseIcon />
+            </span>
+            <ul className="flex-col flex gap-6 sm:gap-12 sm:text-2xl font-bold uppercase pt-4">
+              <li onClick={closeDrawer}>
+                <Link href="/search" aria-label="close sidebar">
+                  Search
+                </Link>
+              </li>
+              <li onClick={closeDrawer}>
+                <Link href="/records">Records</Link>
+              </li>
+              <li onClick={closeDrawer}>
+                <Link href="/contribute">Contribute</Link>
+              </li>
+            </ul>
+            {isContributePage && (
+              <div className="rainbow-kit-connect mt-5">
+                <ConnectButton chainStatus="icon" label="Connect" />
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
